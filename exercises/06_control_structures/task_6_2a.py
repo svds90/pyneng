3be5d6e6b74cbd6b212650c0012a7d9e54c75a5e
svds90 <svds22090@gmail.com>
2022-07-03 07:47:50 +0300

    6.2a

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,32 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip = input('Enter IP: ')
+octets = ip.split('.')
+oct_int = []
+octets_correct = True
+
+if len(octets) != 4:
+    octets_correct = False
+else:
+    for octet in octets:
+        if not(octet.isdigit() and int(octet) in range(256)):
+            octets_correct = False
+            break
+
+if octets_correct == False:
+    print('Неправильный IP-адрес')
+else:
+    oct_int = [int(octet) for octet in octets]
+
+    if oct_int[0] in range(1, 224):
+        print('unicast')
+    elif oct_int[0] in range(224, 239):
+        print('multicast')
+    elif ip == '255.255.255.255':
+        print('local broadcast')
+    elif ip == '0.0.0.0':
+        print('unassigned')
+    else:
+        print('unused')
